255acc03d0364ea9c255d79d3fc24bfdf0228616
Javier Rodriguez <javicerodriguez@gmail.com>
2021-11-12 20:36:18 -0300

    feat: Customize Menu & add react-router-dom

--- src/App.js
+++ src/App.js
@@ -1,12 +1,26 @@
 import React from 'react';
+import Home from './pages/Home'
+import About from './pages/About'
 import 'bootstrap/dist/css/bootstrap.min.css';
+import { Routes, Route } from "react-router-dom";
 import Layout from './theme/layout';
 
 function App() {
   return (
     <div className="App">
       <Layout>
-        adasd
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="about" element={<About />} />
+          <Route
+            path="*"
+            element={
+              <main style={{ padding: "1rem" }}>
+                <p>There's nothing here!</p>
+              </main>
+            }
+          />
+        </Routes>
       </Layout>
     </div>
   );
--- src/components/common/Menu.js
+++ src/components/common/Menu.js
@@ -1,26 +1,27 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import { Nav, Navbar, NavDropdown, Container } from 'react-bootstrap';
 
 const Menu = () => {
   return(
     <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark" sticky="top">
       <Container>
-      <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
+      <Navbar.Brand as={Link} to="/">Recursos418</Navbar.Brand>
       <Navbar.Toggle aria-controls="responsive-navbar-nav" />
       <Navbar.Collapse id="responsive-navbar-nav">
         <Nav className="me-auto">
           <NavDropdown title="Recursos 📚" id="collasible-nav-dropdown">
-            <NavDropdown.Item href="#action/3.1">✅ Frontend</NavDropdown.Item>
-            <NavDropdown.Item href="#action/3.2">✅ Backend</NavDropdown.Item>
-            <NavDropdown.Item href="#action/3.3">✅ Testing</NavDropdown.Item>
+            <NavDropdown.Item as={Link} to="/re/frontend">✅ Frontend</NavDropdown.Item>
+            <NavDropdown.Item as={Link} to="/re/backend">✅ Backend</NavDropdown.Item>
+            <NavDropdown.Item as={Link} to="/re/testing">✅ Testing</NavDropdown.Item>
             <NavDropdown.Divider />
-            <NavDropdown.Item href="#action/3.4">✅ Otros</NavDropdown.Item>
+            <NavDropdown.Item as={Link} to="/re/otros">✅ Otros</NavDropdown.Item>
           </NavDropdown>
         </Nav>
         <Nav>
-          <Nav.Link href="#features">About 👨‍💻</Nav.Link>
-          <Nav.Link href="#pricing">Aportar 🙌</Nav.Link>
-          <Nav.Link href="#pricing">Contacto 😁</Nav.Link>
+          <Nav.Link as={Link} to="/about">About 👨‍💻</Nav.Link>
+          <Nav.Link as={Link} to="/aportar">Aportar 🙌</Nav.Link>
+          <Nav.Link as={Link} to="/contacto">Contacto 😁</Nav.Link>
         </Nav>
       </Navbar.Collapse>
       </Container>
